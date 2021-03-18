a09825004ec2daa1204e89cf87dafbeeb3530313
Nasyrov Sultan <sultan.nasirov@factory5.ai>
2021-03-18 09:28:09 +0300

    Make development setttings

--- ansr/settings.py
+++ ansr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%6n1s!!9uqk3@0)0ozlsn(jribts_-if_at$h7#n00_jv5)^gz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.ansar.su', 'ansar.su', '2.59.40.187']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -132,8 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
